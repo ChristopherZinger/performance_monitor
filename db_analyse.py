d750ecd2ebe31dce20eef635dbcdcc9f55e4c5d5
ChrisropherZinger <krzysztof.zinger@gmail.com>
2020-05-09 13:41:52 +0200

    Added Feature: Pie chart of softwares used during the day.

--- db_analyse.py
+++ db_analyse.py
@@ -21,6 +21,34 @@ def get_week():
             print('Week and year number have to be integer. Try again (week): ')
     return year, week_nr
 
+def convert_timestamp_table_to_date_and_time(df, timestamp_table, new_date_table, new_time_table):
+    def tryconvert(timestamp, timeordate):
+        try:
+            if timeordate=='date':
+                return datetime.datetime.fromtimestamp(timestamp).date()
+            return datetime.datetime.fromtimestamp(timestamp).time()
+        except Exception as e:
+            return None
+
+    df[new_date_table] = df[timestamp_table].apply(
+        lambda timestamp: tryconvert(timestamp,  'date')
+    )
+    df[new_time_table] = df[timestamp_table].apply(
+        lambda timestamp: tryconvert(timestamp,  'time')
+    )
+    return df.drop([timestamp_table,], axis=1)
+
+def timestamp_to_datetime(df, timestamp_table, datetime_table):
+    def tryconvert(timestamp):
+        try:
+            return datetime.datetime.fromtimestamp(timestamp)
+        except Exception as e:
+            return None
+    df[datetime_table] = df[timestamp_table].apply(
+        lambda timestamp: tryconvert(timestamp)
+    )
+    return df
+
 def weekly_key_activity():
     year, week_nr = get_week()
     # generate the week
@@ -87,9 +115,34 @@ def weekly_sortware_usate():
     df = pd.read_sql("""
         SELECT * FROM table_window
         """, conn)
-    print(df)
-    # prepare data to be ploted
-    # plot data
+
+    df['delta_time']=None
+
+    df_delta = pd.DataFrame(columns=['process_name', 'delta_time'])
+
+    for index, row in df.iterrows():
+        df.loc[index, 'delta_time'] =  df.loc[index, 'end_time'] - df.loc[index, 'start_time']
+
+    df['delta_time'] = df.end_time.fillna(0) - df.start_time.fillna(0) # calcualte time of windows active
+    df.loc[ df['delta_time']<0, 'delta_time' ] = 0 # zero if data is missing
+
+    # df.loc[ df['end_time'] == None , 'delta_time'] = 0
+    df = df.groupby('process_name').sum().sort_values('delta_time')
+    print(
+        df.drop(columns=['end_time', 'start_time'])
+    )
+    plot = df.plot(
+        y='delta_time',
+        figsize=(15, 15),
+        title='Softwere usage [%]',
+        kind='pie',
+        fontsize= 6,
+        startangle=90,
+        autopct='%1.1f%%',
+
+        )
+    plt.show()
+
 
 def main():
     #weekly_key_activity()
